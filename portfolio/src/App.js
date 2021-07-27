a21835fe3244aa170a8a5f139d109751b54efda7
Peter Borrow <borrow.peter83@gmail.com>
2021-07-27 13:14:21 +0100

    SETUP: Cleaned filing system

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import React from 'react'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>HELLO</h1>
+    <div>
+      Hello
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
